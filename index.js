b554089230903e8819c4054e74b5e148dfa7dc7c
sunny2831 <sunny2831@hotmail.co.uk>
2018-10-10 09:44:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function happyHolidays(string) {
 }
 
 function happyHolidaysTo(string) {
-  return (`Happy holidays, ${string}!`)
+  return (`Happy ${holidays}, ${string}!`)
 }
 
 function happyHolidaysTo(holiday, name) {
